f8e52cfc6d849dda6ac1b3a45a7b6f67715873ad
Judalm Park (dami) <wnekfa1004@naver.com>
2023-02-12 17:04:16 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 216 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean stuChk[] = new boolean[31];
+        
+        for(int i = 0; i < 28; i ++) {
+            stuChk[sc.nextInt()] = true;
+        }
+        
+        for(int i = 1; i < 31; i++) {
+            if(!stuChk[i]) {
+                System.out.println(i);
+            }
+                   
+        }
+    }
+}
