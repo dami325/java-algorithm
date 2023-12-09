13deebbd5a52b1718542d3c7c111877dd1039082
Judalm Park (dami) <wnekfa1004@naver.com>
2023-12-09 22:18:02 +0900

    [Bronze II] Title: 진법 변환, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        String B = sc.next();
+
+        System.out.println(Integer.parseInt(N, Integer.parseInt(B)));
+
+    }
+
+}
