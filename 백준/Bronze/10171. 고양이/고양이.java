d7c1a4747bdd78d1fc35e625f3650eddf5741b21
Judalm Park (dami) <wnekfa1004@naver.com>
2023-12-31 19:56:04 +0900

    [Bronze V] Title: 고양이, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * 문제
+         * 아래 예제와 같이 고양이를 출력하시오.
+         *
+         * 입력
+         * 없음.
+         *
+         * 출력
+         * 고양이를 출력한다.
+         *
+         * 예제 입력 1
+         * 예제 출력 1
+         * \    /\
+         *  )  ( ')
+         * (  /  )
+         *  \(__)|
+         */
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
