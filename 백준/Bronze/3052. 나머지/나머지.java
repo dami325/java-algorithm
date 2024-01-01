a6eeedd3273ddc5280bb764b96405d72c5440a10
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-01 16:08:38 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Set<Integer> list = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(solve(br.readLine()));
+        }
+
+        System.out.println(list.size());
+
+
+    }
+
+    public static int solve(String str) {
+        return Integer.parseInt(str)%42;
+    }
+}
