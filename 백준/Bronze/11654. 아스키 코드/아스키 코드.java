a0afdf9d5e88eb023de40466f93302d90229256f
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-05 21:43:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String next = new Scanner(System.in).next();
+
+        char c = next.charAt(0);
+        System.out.println((int)c);
+    }
+}
