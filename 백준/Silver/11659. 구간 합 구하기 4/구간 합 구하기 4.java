9bb69f6ce64b527fb07f6c205a2b0c98614f42f9
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-14 00:02:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2480 ms, Memory: 256208 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -6,27 +6,16 @@ public class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        int[] arr = new int[N];
-        int[] sumArr = new int[N];
+        int[] sumArr = new int[N + 1];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-            if (i == 0) {
-                sumArr[i] = arr[i];
-            } else {
-                sumArr[i] = sumArr[i-1] + arr[i];
-            }
+        for (int i = 1; i <= N; i++) {
+            sumArr[i] = sumArr[i - 1 ] + sc.nextInt();
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= M; i++) {
             int start = sc.nextInt();
             int end = sc.nextInt();
-
-            if (start == 1) {
-                System.out.println(sumArr[end-1]);
-            } else {
-                System.out.println(sumArr[end-1] - sumArr[start - 2]);
-            }
+            System.out.println(sumArr[end] - sumArr[start - 1]);
         }
 
     }
