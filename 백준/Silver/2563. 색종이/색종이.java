f40ad1a3934ae0c816fd582a45b4feaa5d4fe5e5
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-14 14:26:29 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = null;
+
+        boolean[][] arr = new boolean[100][100];
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        result++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
