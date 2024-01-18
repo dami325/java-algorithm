95f7553597ff41676233d59baf2eedc4b0ba4c32
Judalm Park (dami) <wnekfa1004@naver.com>
2024-01-18 19:32:24 +0900

    [Bronze II] Title: 벌집, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int start = 1;
+
+        for (int i = 1; i <= N; i++) {
+
+            if (N <= start) {
+                System.out.println(i);
+                break;
+            }
+
+            start += 6 * i;
+
+        }
+
+    }
+}
