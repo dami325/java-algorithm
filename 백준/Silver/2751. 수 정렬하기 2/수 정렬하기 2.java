503c809978128970599c2c6a555c788a7bf48b55
Judalm Park (dami) <wnekfa1004@naver.com>
2024-05-20 08:58:54 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1620 ms, Memory: 119680 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        ) {
+
+            int n = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(br.readLine());
+            }
+
+            Arrays.sort(arr);
+
+            for (int i = 0; i < n; i++) {
+                bw.write(arr[i] + "\n");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
