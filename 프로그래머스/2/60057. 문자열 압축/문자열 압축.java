c3ad1031287f3570d942ed341862d2008e6cdb7c
Judalm Park (dami) <wnekfa1004@naver.com>
2024-07-25 22:46:47 +0900

    [level 2] Title: 문자열 압축, Time: 404.94 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,6 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class Solution {
+
     public int solution(String s) {
-        int answer = 0;
-        return answer;
+
+        int length = s.length();
+
+
+        for (int size = 1; size <= s.length() / 2; size++) {
+            Queue<String> q = new ArrayDeque<>();
+            for (int j = 0; j <= s.length() - size; j += size) {
+                String substring = s.substring(j, j + size);
+                q.offer(substring);
+            }
+
+            String result = s;
+            while (!q.isEmpty()) {
+                int count = 1;
+                String left = q.poll();
+                while (left.equals(q.peek())) {
+                    left = q.poll();
+                    count++;
+                }
+                if (count > 1) {
+                    String replace = "";
+                    for (int i = 0; i < count; i++) {
+                        replace += left;
+                    }
+                    result = result.replaceFirst(replace, count + left);
+                }
+            }
+
+            length = length > result.length() ? result.length() : length;
+
+        }
+        return length;
     }
+
 }
