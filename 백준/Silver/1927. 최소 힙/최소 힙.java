ecdac24d72802e596fe711a63582393e107c55b7
Judalm Park (dami) <wnekfa1004@naver.com>
2024-07-31 00:46:32 +0900

    [Silver II] Title: 최소 힙, Time: 1124 ms, Memory: 32624 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Comparator<Long> comparator = Comparator.comparingLong(o -> o);
+        PriorityQueue<Long> q = new PriorityQueue<>(comparator);
+
+        int n = Integer.parseInt(br.readLine());
+
+        while (n-- > 0) {
+            long num = Long.parseLong(br.readLine());
+            if (num == 0) {
+                if(!q.isEmpty()){
+                    System.out.println(q.poll());
+                } else {
+                    System.out.println(0);
+                }
+            }else{
+                q.add(num);
+            }
+        }
+
+        br.close();
+
+    }
+}
