60a0304b3bac724a99d3f9b85efe6872ff8de5ee
Judalm Park (dami) <wnekfa1004@naver.com>
2024-08-29 18:43:28 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 4.44 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,41 +1,34 @@
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 class Solution {
 
-    private Map<Long, Integer> rankMap = Map.of(
-            0L,6,
-            1L,6,
-            2L,5,
-            3L,4,
-            4L,3,
-            5L,2,
-            6L,1
+    private Map<Integer, Integer> rankMap = Map.of(
+            0, 6,
+            1, 6,
+            2, 5,
+            3, 4,
+            4, 3,
+            5, 2,
+            6, 1
     );
+
     public int[] solution(int[] lottos, int[] win_nums) {
 
-        Set<Integer> winNumSet = Arrays.stream(win_nums)
-                .boxed()
-                .collect(Collectors.toSet());
-
-        Set<Integer> lottoSet = Arrays.stream(lottos)
-                .filter(value -> value != 0)
-                .boxed()
-                .collect(Collectors.toSet());
-
-        Long zeroCount = Arrays.stream(lottos)
+        int zeroCount = (int) Arrays.stream(lottos)
                 .filter(value -> value == 0)
                 .count();
 
-        Long sameCount = lottoSet.stream()
-                .filter(winNumSet::contains)
+        int sameCount = (int) Arrays.stream(lottos)
+                .filter(value -> value != 0)
+                .filter(
+                        lottoNum -> Arrays.stream(win_nums).anyMatch(winNum -> winNum == lottoNum)
+                )
                 .count();
 
         int lowestRanking = rankMap.get(sameCount);
         int highestRanking = rankMap.get(sameCount + zeroCount);
-        
+
         return new int[]{highestRanking, lowestRanking};
     }
 
