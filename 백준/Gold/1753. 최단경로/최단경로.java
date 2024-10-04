b5295dfbe8ba4c8040712da0ce66a01a94635536
Judalm Park (dami) <wnekfa1004@naver.com>
2024-10-04 20:02:47 +0900

    [Gold IV] Title: 최단경로, Time: 680 ms, Memory: 108956 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,85 +1,97 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int v = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int startNode = Integer.parseInt(st.nextToken());
+
+            ArrayList<Node>[] graphs = new ArrayList[v + 1];
+            int[] resultArr = new int[v + 1];
+            boolean[] visited = new boolean[v + 1];
+
+            Arrays.fill(resultArr, Integer.MAX_VALUE);
+
+            for (int i = 1; i <= v; i++) {
+                graphs[i] = new ArrayList<>();
+            }
+
+            for (int i = 0; i < e; i++) {
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                int value = Integer.parseInt(st.nextToken());
+
+                graphs[start].add(new Node(end, value));
+            }
+
+            resultArr[startNode] = 0;  // startNode에 0을 설정
+            PriorityQueue<Node> queue = new PriorityQueue<>();
+            queue.add(new Node(startNode, 0));
+
+            while (!queue.isEmpty()) {
+                Node qNode = queue.poll();
+                int current = qNode.getEnd();
+                if(visited[current])
+                    continue;
+                visited[current] = true;
+
+                for (Node node : graphs[current]) {
+                    int end = node.getEnd();
+                    int weight = node.getWeight();
+                    if(resultArr[end] > resultArr[current] + weight){
+                        resultArr[end] = resultArr[current] + weight;
+                        queue.add(new Node(end, resultArr[end]));
+                    }
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i < resultArr.length; i ++) {
+                if (resultArr[i] == Integer.MAX_VALUE) {
+                    sb.append("INF\n");
+                }else{
+                    sb.append(resultArr[i] + "\n");
+                }
+            }
+
+            System.out.println(sb.toString());
+
+        }
+    }
+}
 
 class Node implements Comparable<Node>{
-    int end, weight;
+    int end;
+    int weight;
 
-    public Node(int end, int weight){
+    public Node(int end, int weight) {
         this.end = end;
         this.weight = weight;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getWeight() {
+        return weight;
     }
 
     @Override
     public int compareTo(Node o) {
         return weight - o.weight;
-    }
-}
-
-public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final int INF = 100_000_000;
-    static int v,e,k;
-    static List<Node>[] list;
-    static int[] dist;
-
-
-    public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        v = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(br.readLine());
-        list = new ArrayList[v + 1];
-        dist = new int[v + 1];
-
-        Arrays.fill(dist, INF);
-
-        for(int i = 1; i <= v; i++){
-            list[i] = new ArrayList<>();
-        }
-        // 리스트에 그래프 정보를 초기화
-        for(int i = 0 ; i < e; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            // start에서 end로 가는 weight 가중치
-            list[start].add(new Node(end, weight));
-        }
-
-        StringBuilder sb = new StringBuilder();
-        // 다익스트라 알고리즘
-        dijkstra(k);
-        // 출력 부분
-        for(int i = 1; i <= v; i++){
-            if(dist[i] == INF) sb.append("INF\n");
-            else sb.append(dist[i] + "\n");
-        }
-
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
-    }
-
-    private static void dijkstra(int start){
-       PriorityQueue<Node> queue = new PriorityQueue<>();
-       boolean[] check = new boolean[v + 1];
-       queue.add(new Node(start, 0));
-       dist[start] = 0;
-
-       while(!queue.isEmpty()){
-           Node curNode = queue.poll();
-           int cur = curNode.end;
-
-           if(check[cur] == true) continue;
-           check[cur] = true;
-
-           for(Node node : list[cur]){
-               if(dist[node.end] > dist[cur] + node.weight){
-                   dist[node.end] = dist[cur] + node.weight;
-                   queue.add(new Node(node.end, dist[node.end]));
-               }
-           }
-       }
     }
 }
