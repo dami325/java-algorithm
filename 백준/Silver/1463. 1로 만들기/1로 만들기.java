5f99d9b327931d8560a78ad9f6060fab94152d57
Judalm Park (dami) <wnekfa1004@naver.com>
2024-10-27 01:14:17 +0900

    [Silver III] Title: 1로 만들기, Time: 304 ms, Memory: 78112 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,42 +1,32 @@
 import java.util.Scanner;
+
 public class Main {
- 
-	static Integer[] dp;
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
- 
-		dp = new Integer[N + 1];
-		dp[0] = dp[1] = 0;
- 
-		System.out.print(recur(N));
- 
-	}
- 
-	static int recur(int N) {
- 
-		if (dp[N] == null) {
-			// 6으로 나눠지는 경우 
-			if (N % 6 == 0) {
-				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-			}
-			// 3으로만 나눠지는 경우 
-			else if (N % 3 == 0) {
-				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-			}
-			// 2로만 나눠지는 경우 
-			else if (N % 2 == 0) {
-				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-			}
-			// 2와 3으로 나누어지지 않는 경우
-			else {
-				dp[N] = recur(N - 1) + 1;
-			}
-		}
-		return dp[N];
-	}
- 
+
+    private static Integer[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        dp = new Integer[x + 1];
+
+        dp[0] = dp[1] = 0;
+
+        System.out.println(topDown(x));
+    }
+
+    private static int topDown(int x) {
+
+        if (dp[x] == null) {
+            if (x % 6 == 0) {
+                dp[x] = Math.min(topDown(x - 1), Math.min(topDown(x / 3), topDown(x / 2))) + 1;
+            } else if (x % 3 == 0) {
+                dp[x] = Math.min(topDown(x / 3), topDown(x - 1)) + 1;
+            } else if (x % 2 == 0) {
+                dp[x] = Math.min(topDown(x / 2), topDown(x - 1)) + 1;
+            } else {
+                dp[x] = topDown(x - 1) + 1;
+            }
+        }
+        return dp[x];
+    }
 }
