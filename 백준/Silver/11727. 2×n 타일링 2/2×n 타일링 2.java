ec57c104ebbd7711efcca83307833497daea0b50
Judalm Park (dami) <wnekfa1004@naver.com>
2025-01-15 00:03:11 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 120 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+/**
+ * 길이 2의 직사각형을 3가지( 1, 2, 2)의 타일로 채우는 방법의 수..
+ * 2x1     2x2     2x3     2x4    2x5    2x6     2x7     2x8
+ * dp[1]=1 dp[2]=3  dp[1]+dp[2] ...
+ */
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int n = scanner.nextInt();
-		int [] Dp = new int[n+1];
-		Dp[0]=1; // 2*1 사각형 경우의 수
-		Dp[1]=3; // 2*2 사각형 경우의 수
-		for(int i=2;i<n;i++) {
-			Dp[i]=(Dp[i-1]+Dp[i-2]*2)%10007; 
-		}
-		System.out.println(Dp[n-1]); //2*n 사각형 경우의 수
-
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 3;
+        for (int i = 2; i < n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+        }
+        System.out.println(dp[n - 1]);
+    }
 }
